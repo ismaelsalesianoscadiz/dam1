b232bff39ccd3dcd1e4f36e8f08b04249ecb4c66
1-DAM <1-DAM@Aula2-PC23>
2014-02-07 10:48:03 +0100

    Insercion de operacion y nombre Manuel.

--- src/pryecto_versiones/Pryecto_versiones.java
+++ src/pryecto_versiones/Pryecto_versiones.java
@@ -1,25 +1,16 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pryecto_versiones;
 
-/**
- *
- * @author 1-DAM
- */
 public class Pryecto_versiones {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       
-        
-       System.out.println("hola");
-       
+
+        System.out.println("hola");
         System.out.println("Ismael Abad");
-        
-            System.out.println("Ismael2");
+        System.out.println("Ismael2");
+
+        //Manu crea comentario y operación
+        System.out.println("Manuel S. Gomez");
+        int x = 2, y = 5;
+        System.out.println("Operación: "+(x*y));
     }
 }
