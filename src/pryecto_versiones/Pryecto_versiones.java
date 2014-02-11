019e2cc278f92cc3fd9cf9c9565ed45da46ae7fe
1-DAM <1-DAM@Aula2-PC01>
2014-02-11 09:20:11 +0100

    jaajajajj*nelson*

--- src/pryecto_versiones/Pryecto_versiones.java
+++ src/pryecto_versiones/Pryecto_versiones.java
@@ -11,9 +11,9 @@ public class Pryecto_versiones {
         //Manu crea comentario
         System.out.println("Manuel S. Gomez soy delegado corrupto");
         System.out.println("Antonio J. Beltrán Muñoz soy la desgracia de mi fila");
-        System.out.println("Sebastián Utrera Bonet soy EL NUM 1");
+        System.out.println("Sebastián Utrera Bonet multiplicame por 0");
         System.out.println("Eduardo Formanti Alonso soy un sirujano de tremenda calidad");
-        System.out.println("DEJARSE DE TONTERIA QUE ME LEVANTO");
+        System.out.println("DEJARSE DE TONTERIA QUE ME La LEVANTO");
         System.out.println("ESTUDIAR FOL CABRONES");
         System.out.println("David Gonzalez Nisa NEGROORRORORF");
     }
