70bb76d72421323431b02760459875b3dacbab6b
1-DAM <1-DAM@Aula2-PC09>
2014-02-11 08:56:49 +0100

    nuevo cambio

--- src/pryecto_versiones/Pryecto_versiones.java
+++ src/pryecto_versiones/Pryecto_versiones.java
@@ -9,9 +9,10 @@ public class Pryecto_versiones {
         System.out.println("Ismael2");
 
         //Manu crea comentario
-        System.out.println("Manuel S. Gomez soy delegado");
-        System.out.println("Antonio J. Beltrán Muñoz soy muy pero que muy tonto");
-        System.out.println("Sebastián Utrera Bonet");
-        System.out.println("Eduardo Formanti Alonso");
+        System.out.println("Manuel S. Gomez soy delegado corrupto");
+        System.out.println("Antonio J. Beltrán Muñoz soy muy Listo");
+        System.out.println("Sebastián Utrera Bonet soy el peor");
+        System.out.println("Eduardo Formanti Alonso soy un piltrafilla");
+        
     }
 }
