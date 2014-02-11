a77725999b9ef7936a8ec269aa0ac9072c1257a8
1-DAM <1-DAM@Aula2-PC01>
2014-02-11 09:39:27 +0100

    david

--- src/pryecto_versiones/Pryecto_versiones.java
+++ src/pryecto_versiones/Pryecto_versiones.java
@@ -9,12 +9,14 @@ public class Pryecto_versiones {
         System.out.println("Ismael2");
 
         //Manu crea comentario
-        System.out.println("Manuel S. Gomez soy delegado corrupto");
+        System.out.println("Manuel S. Gomez soy delegado corrupto =3");
         System.out.println("Antonio J. Beltrán Muñoz soy la desgracia de mi fila");
         System.out.println("Sebastián Utrera Bonet, ;)");
         System.out.println("Eduardo Formanti Alonso soy un sirujano de dudusa calidad");
         System.out.println("DEJARSE DE TONTERIA QUE ME La LEVANTO");
         System.out.println("ESTUDIAR FOL CABRONES");
-        System.out.println("David Gonzalez Nisa Barto... Yo invito!");
+        System.out.println("David Gonzalez Nisa Barto...!");
+        System.out.println("mm si la cria del perro, se llama perrito.. y la del gato.. gatito... como se llama la de yoinvo?");
+        System.out.println("Respuesta: _____");
     }
 }
