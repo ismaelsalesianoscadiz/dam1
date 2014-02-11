ae4326b5b45daed708a17117c992e492d339d39d
1-DAM <1-DAM@Aula2-PC23>
2014-02-11 08:49:55 +0100

    Manu añade "2"

--- src/pryecto_versiones/Pryecto_versiones.java
+++ src/pryecto_versiones/Pryecto_versiones.java
@@ -9,9 +9,9 @@ public class Pryecto_versiones {
         System.out.println("Ismael2");
 
         //Manu crea comentario
-        System.out.println("Manuel S. Gomez soy delegado");
-        System.out.println("Antonio J. Beltrán Muñoz soy tonto");
-        System.out.println("Sebastián Utrera Bonet");
-        System.out.println("Eduardo Formanti Alonso");
+        System.out.println("Manuel S. Gomez soy delegado2");
+        System.out.println("Antonio J. Beltrán Muñoz soy tonto2");
+        System.out.println("Sebastián Utrera Bonet2");
+        System.out.println("Eduardo Formanti Alonso2");
     }
 }
